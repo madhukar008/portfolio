58f9fcd34aeb01aad7ac5dc8678becd89104b94a
Madhukar Shukla <madhukar251099@gmail.com>
2024-01-11 01:51:21 +0530

    project responsive

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,8 +6,8 @@ export const ProjectCard = ({ title, description, imgUrl, tech, link }) => {
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+          <h4 className="mb-2">{title}</h4>
+          <p className="mb-3">{description}</p>
           <a href={link} className="text-white h2" target="_blank">
           <ul className="list-unstyled d-flex align-items-center justify-content-center gap-2 icon-wrap">
             {tech.map((el, idx)=>{
